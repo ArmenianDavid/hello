fde878c38e896f90966c57b4a9f489264bc9a746
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-07 17:06:42 +0400

    Feat add pagiation functionality

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 // components
 import "./List.css";
 import SingleQuestion from "../singleQuestion/SingleQuestion";
+import Pagination from "../Pagination/Pagination";
+
+const QUESTIONS_PER_PAGE = 1;
 
 const List = ({ data, getCheckedAnswers }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const indexOfLastQuestion = currentPage * QUESTIONS_PER_PAGE;
+    const indexOfFirstQuestion = indexOfLastQuestion - QUESTIONS_PER_PAGE;
+    const totalPages = Math.ceil(data.length / QUESTIONS_PER_PAGE);
+    const currentQuerstions = data.slice(
+        indexOfFirstQuestion,
+        indexOfLastQuestion
+    );
+
+    const handlePaginationChange = (event, value) => {
+        setCurrentPage(value);
+    };
+
     return (
         <div className="list-container">
-            {data.map((item) => {
-                return (
-                    <SingleQuestion
-                        key={item.id}
-                        id={item.id}
-                        img={item.img}
-                        answers={item.answers}
-                        getCheckedAnswers={getCheckedAnswers}
-                    />
-                );
-            })}
+            <div>
+                {currentQuerstions.map((item) => {
+                    return (
+                        <SingleQuestion
+                            key={item.id}
+                            id={item.id}
+                            img={item.img}
+                            answers={item.answers}
+                            getCheckedAnswers={getCheckedAnswers}
+                        />
+                    );
+                })}
+            </div>
+            <div>
+                <Pagination
+                    totalPages={totalPages}
+                    handlePaginationChange={handlePaginationChange}
+                />
+            </div>
         </div>
     );
 };
