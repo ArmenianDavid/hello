8b18f0ce8d41adf8ce57bfa04fc98f07843aca8b
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-07 17:04:38 +0400

    Refactor index and Header components

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+// components
+import "./Header.css";
 
 const Header = (props) => {
     return (
--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// components
+import App from "./App";
+import "./Index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Header from './Components/Header'
-import List from './Components/List'
-import './Index.css';
-
-const App = () =>{
-  return(
-    <div>
-         <Header />
-
-         <List />
-
-    </div>
-  )
-}
-
-ReactDOM.render( <App /> , document.getElementById('root'))
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
